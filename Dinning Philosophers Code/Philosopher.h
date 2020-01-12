e8dd8334135dd3735a92968d535272dbdb4e10a0
JamesFinglas <32223005+JamesFinglas@users.noreply.github.com>
2020-01-12 03:15:32 +0000

    Add files via upload

---
+++ Dinning Philosophers Code/Philosopher.h
@@ -0,0 +1,23 @@
+//------------------------------------------------------------------------
+// Filename: Philosopher.h
+//   Interface file for class Philosopher
+//------------------------------------------------------------------------
+ 
+#ifndef _PHILOSOPHER_H
+#define _PHILOSOPHER_H
+
+#include "ThreadClass.h"
+
+#define PHILOSOPHERS     5
+
+class Philosopher: public Thread 
+{
+     public:
+          Philosopher(int Number, int iter);
+     private:
+          int No;
+          int Iterations;
+          void ThreadFunc();
+};
+
+#endif
